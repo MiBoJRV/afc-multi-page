05bd176e07c8a3cb8145f9d085de3db96123f5fc
Mykhailo Bodnar <mykhailo.bodnar@wetelo.com>
2024-03-19 15:15:03 +0100

    added Testimonials page

---
+++ scripts/swiper.js
@@ -0,0 +1,19 @@
+let swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    spaceBetween: 40,
+    loopedSlides: 2,
+    slidesPerView: 3,
+    loop: true,
+    centeredSlides: true,
+    centeredSlidesBounds: true
+});
+
+
+
+
+
+
+
